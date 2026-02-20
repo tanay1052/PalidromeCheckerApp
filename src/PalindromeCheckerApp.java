5991d7dce1a9c3b9dcb7eb1d901764eb251e45c9
tanay1052 <tanaypatel2808@gmail.com>
2026-02-20 11:14:51 +0530

    UC4

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,25 +1,35 @@
 public class PalindromeCheckerApp {
 
     /**
-     @@ -31,15 +32,14 @@ public class PalindromeCheckerApp {
+     * Application entry point for UC2.
+     *
+     * @param args Command-line arguments
      */
     public static void main(String[] args) {
         String input = "madam";
-        boolean result = true;
-        for (int i = 0; i < input.length() / 2; i++) {
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                result = false;
-                break;
-            }
-            System.out.println("Input text: " + input);
-            String reverse = "";
-            for (int i = input.length() - 1; i >= 0; i--) {
-                reverse += input.charAt(i);
+        System.out.println("Input text: " + input);
+        String reverse = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reverse += input.charAt(i);
+
+            char[] chars = input.toCharArray();
+            int left = 0;
+            int right = chars.length - 1;
+            boolean isPalindrome = true;
+
+            while (left < right) {
+                if (chars[left] != chars[right]) {
+                    isPalindrome = false;
+                    break;
+                }
+                left++;
+                right--;
             }
 
-            System.out.println("Is it a Palindrome?: " + result);
             System.out.print("It it a Palindrome? : ");
             System.out.println(input.equals(reverse));
+            System.out.print("It is a Palindrome? : ");
+            System.out.println(isPalindrome);
         }
 
     }
