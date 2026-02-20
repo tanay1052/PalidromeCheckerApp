f5eca4aab66c8afeb5e325db52f010fa26700df0
tanay1052 <tanaypatel2808@gmail.com>
2026-02-20 11:10:24 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,9 +1,7 @@
 public class PalindromeCheckerApp {
 
     /**
-     * Application entry point for UC2.
-     *
-     * @param args Command-line arguments
+     @@ -31,15 +32,14 @@ public class PalindromeCheckerApp {
      */
     public static void main(String[] args) {
         String input = "madam";
@@ -13,9 +11,15 @@ public class PalindromeCheckerApp {
                 result = false;
                 break;
             }
+            System.out.println("Input text: " + input);
+            String reverse = "";
+            for (int i = input.length() - 1; i >= 0; i--) {
+                reverse += input.charAt(i);
+            }
+
+            System.out.println("Is it a Palindrome?: " + result);
+            System.out.print("It it a Palindrome? : ");
+            System.out.println(input.equals(reverse));
         }
 
-        System.out.println("Is it a Palindrome?: " + result);
     }
-
-}
