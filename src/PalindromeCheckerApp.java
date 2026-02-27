6a436f46e8c61f7c6e5417f800af23b78103a95e
tanay1052 <tanaypatel2808@gmail.com>
2026-02-27 09:44:14 +0530

    UC11

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,53 +1,40 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Scanner;
 
-/**
- * ============================================================
- * MAIN CLASS - UseCase7PalindromeCheckerApp
- * ============================================================
- * <p>
- * Use Case 7: Deque-Based Optimized Palindrome Checker
- * <p>
- * Description:
- * This class validates a palindrome using a Deque
- * (Double Ended Queue).
- * <p>
- * Characters are inserted into the deque and compared
- * by removing elements from both ends.
- * <p>
- * Data Structure Used: Deque (ArrayDeque)
- *
- * @author Developer
- * @version 7.0
- */
+class PalindromeChecker {
+    public boolean checkPalindrome(String input) {
+        if (input == null) {
+            return false;
+        }
+
+        int start = 0;
+        int end = input.length() - 1;
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
 
 public class PalindromeCheckerApp {
-
-    /**
-     * Application entry point for UC7.
-     *
-     * @param args Command-line arguments
-     */
     public static void main(String[] args) {
-        String input = "refer";
+        Scanner scanner = new Scanner(System.in);
 
-        Deque<Character> deque = new ArrayDeque<>();
-        for (char c : input.toCharArray()) {
-            deque.addLast(c);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        PalindromeChecker service = new PalindromeChecker();
+        boolean result = service.checkPalindrome(input);
+
+        if (result) {
+            System.out.println("Result: TRUE (It is a palindrome)");
+        } else {
+            System.out.println("Result: FALSE (Not a palindrome)");
         }
 
-        boolean isPalindrome = true;
-        while (deque.size() > 1) {
-            char front = deque.removeFirst();
-            char rear = deque.removeLast();
-
-            if (front != rear) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        System.out.println("Input: " + input);
-        System.out.println("Is Palindrome?: " + isPalindrome);
+        scanner.close();
     }
 }
