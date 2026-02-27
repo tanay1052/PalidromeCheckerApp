817a8efe3784bec96312a698e7c4683242e81041
tanay1052 <tanaypatel2808@gmail.com>
2026-02-27 09:59:37 +0530

    UC3

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,82 +1,25 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class PalindromeCheckerApp {
 
-    // Two Pointer Approach (O(1) space)
-    public static boolean twoPointerCheck(String input) {
-        int start = 0;
-        int end = input.length() - 1;
-
-        while (start < end) {
-            if (input.charAt(start) != input.charAt(end)) {
-                return false;
-            }
-            start++;
-            end--;
-        }
-        return true;
-    }
-
-    // Stack Approach (O(n) space)
-    public static boolean stackCheck(String input) {
-        Stack<Character> stack = new Stack<>();
-
-        for (char c : input.toCharArray()) {
-            stack.push(c);
-        }
-
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    // Deque Approach (O(n) space)
-    public static boolean dequeCheck(String input) {
-        Deque<Character> deque = new ArrayDeque<>();
-
-        for (char c : input.toCharArray()) {
-            deque.addLast(c);
-        }
-
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast())) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a string: ");
-        String input = scanner.nextLine();
-        long startTime = System.nanoTime();
-        boolean result1 = twoPointerCheck(input);
-        long endTime = System.nanoTime();
-        long duration1 = endTime - startTime;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String Text = sc.nextLine();
 
-        startTime = System.nanoTime();
-        boolean result2 = stackCheck(input);
-        endTime = System.nanoTime();
-        long duration2 = endTime - startTime;
+        String reversed = "";
 
-        startTime = System.nanoTime();
-        boolean result3 = dequeCheck(input);
-        endTime = System.nanoTime();
-        long duration3 = endTime - startTime;
+        for(int i = Text.length() - 1; i >= 0; i--) {
+            reversed += Text.charAt(i);
+        }
 
-        System.out.println("\nResults:");
-        System.out.println("Two Pointer Result: " + result1 + " | Time: " + duration1 + " ns");
-        System.out.println("Stack Result:       " + result2 + " | Time: " + duration2 + " ns");
-        System.out.println("Deque Result:       " + result3 + " | Time: " + duration3 + " ns");
+        if(Text.equals(reversed)) {
+            System.out.println("It is a Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
 
-        scanner.close();
+        sc.close();
     }
 }
